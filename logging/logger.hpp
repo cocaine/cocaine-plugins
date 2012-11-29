73bc38760b945b040441524adcf8fe27f1dc6775
Andrey Sibiryov <me@kobology.ru>
2012-11-29 21:14:09 +0400

    missing files

---
+++ logging/logger.hpp
@@ -0,0 +1,54 @@
+/*
+    Copyright (c) 2011-2012 Andrey Sibiryov <me@kobology.ru>
+    Copyright (c) 2011-2012 Other contributors as noted in the AUTHORS file.
+
+    This file is part of Cocaine.
+
+    Cocaine is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Cocaine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+#ifndef COCAINE_REMOTE_LOGGER_HPP
+#define COCAINE_REMOTE_LOGGER_HPP
+
+#include "service.hpp"
+
+#include <cocaine/api/client.hpp>
+#include <cocaine/api/logger.hpp>
+
+namespace cocaine { namespace logger {
+
+class remote_t:
+    public api::logger_t
+{
+    public:
+        typedef api::logger_t category_type;
+
+    public:
+        remote_t(context_t& context,
+                 const std::string& name,
+                 const Json::Value& args);
+
+        virtual
+        void
+        emit(logging::priorities priority,
+             const std::string& source,
+             const std::string& message);
+
+    private:
+        api::client<io::tags::logging_tag> m_client;
+};
+
+}} // namespace cocaine::sink
+
+#endif
