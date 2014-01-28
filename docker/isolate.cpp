86b2a35761c250931211ab022d67c3d29bc5f5fc
andrusha97 <andrey.goryachev@gmail.com>
2014-01-28 19:12:34 +0400

    Throw proper exceptions from docker isolation.
    Cocaine Runtime expects cocaine::error_t exceptions from all components.

--- docker/isolate.cpp
+++ docker/isolate.cpp
@@ -113,54 +113,58 @@ docker_t::docker_t(context_t& context, const std::string& name, const Json::Valu
         m_log
     )
 {
-    m_runtime_path = args.get("runtime-path", "/run/cocaine").asString();
+    try {
+        m_runtime_path = args.get("runtime-path", "/run/cocaine").asString();
 
-    if(args.isMember("registry")) {
-        m_do_pool = true;
+        if(args.isMember("registry")) {
+            m_do_pool = true;
 
-        // <default> means default docker's registry (the one owned by dotCloud). User must specify it explicitly.
-        // I think that if there is no registry in the config then the plugin must not pull images from foreign registry by default.
-        if(args["registry"].asString() != "<default>") {
-            m_image += args["registry"].asString() + "/";
+            // <default> means default docker's registry (the one owned by dotCloud). User must specify it explicitly.
+            // I think that if there is no registry in the config then the plugin must not pull images from foreign registry by default.
+            if(args["registry"].asString() != "<default>") {
+                m_image += args["registry"].asString() + "/";
+            }
+        } else {
+            COCAINE_LOG_WARNING(m_log, "Docker registry is not specified. Only local images will be used.");
         }
-    } else {
-        COCAINE_LOG_WARNING(m_log, "Docker registry is not specified. Only local images will be used.");
+
+        if(args.isMember("repository")) {
+            m_image += args["repository"].asString() + "/";
+        }
+        m_image += name;
+        m_tag = ""; // empty for now
+
+        m_run_config.SetObject();
+
+        m_run_config.AddMember("Hostname", "", m_json_allocator);
+        m_run_config.AddMember("User", "", m_json_allocator);
+        m_run_config.AddMember("Memory", int64_t(args.get("memory_limit", 0).asInt64()), m_json_allocator);
+        m_run_config.AddMember("MemorySwap", int64_t(args.get("memory_swap", 0).asInt64()), m_json_allocator);
+        m_run_config.AddMember("CpuShares", int64_t(args.get("cpu_shares", 0).asInt64()), m_json_allocator);
+        m_run_config.AddMember("AttachStdin", false, m_json_allocator);
+        m_run_config.AddMember("AttachStdout", false, m_json_allocator);
+        m_run_config.AddMember("AttachStderr", false, m_json_allocator);
+        rapidjson::Value v1;
+        m_run_config.AddMember("PortSpecs", v1, m_json_allocator);
+        m_run_config.AddMember("Tty", false, m_json_allocator);
+        m_run_config.AddMember("OpenStdin", false, m_json_allocator);
+        m_run_config.AddMember("StdinOnce", false, m_json_allocator);
+        rapidjson::Value v2(rapidjson::kArrayType);
+        m_run_config.AddMember("Env", v2, m_json_allocator);
+        rapidjson::Value v3(rapidjson::kArrayType);
+        m_run_config.AddMember("Cmd", v3, m_json_allocator);
+        rapidjson::Value v4;
+        m_run_config.AddMember("Dns", v4, m_json_allocator);
+        m_run_config.AddMember("Image", m_image.data(), m_json_allocator);
+        rapidjson::Value v5(rapidjson::kObjectType);
+        m_run_config.AddMember("Volumes", v5, m_json_allocator);
+        rapidjson::Value empty_object(rapidjson::kObjectType);
+        m_run_config["Volumes"].AddMember(m_runtime_path.c_str(), empty_object, m_json_allocator);
+        m_run_config.AddMember("VolumesFrom", "", m_json_allocator);
+        m_run_config.AddMember("WorkingDir", "/", m_json_allocator);
+    } catch(const std::exception& e) {
+        throw cocaine::error_t("%s", e.what());
     }
-
-    if(args.isMember("repository")) {
-        m_image += args["repository"].asString() + "/";
-    }
-    m_image += name;
-    m_tag = ""; // empty for now
-
-    m_run_config.SetObject();
-
-    m_run_config.AddMember("Hostname", "", m_json_allocator);
-    m_run_config.AddMember("User", "", m_json_allocator);
-    m_run_config.AddMember("Memory", int64_t(args.get("memory_limit", 0).asInt64()), m_json_allocator);
-    m_run_config.AddMember("MemorySwap", int64_t(args.get("memory_swap", 0).asInt64()), m_json_allocator);
-    m_run_config.AddMember("CpuShares", int64_t(args.get("cpu_shares", 0).asInt64()), m_json_allocator);
-    m_run_config.AddMember("AttachStdin", false, m_json_allocator);
-    m_run_config.AddMember("AttachStdout", false, m_json_allocator);
-    m_run_config.AddMember("AttachStderr", false, m_json_allocator);
-    rapidjson::Value v1;
-    m_run_config.AddMember("PortSpecs", v1, m_json_allocator);
-    m_run_config.AddMember("Tty", false, m_json_allocator);
-    m_run_config.AddMember("OpenStdin", false, m_json_allocator);
-    m_run_config.AddMember("StdinOnce", false, m_json_allocator);
-    rapidjson::Value v2(rapidjson::kArrayType);
-    m_run_config.AddMember("Env", v2, m_json_allocator);
-    rapidjson::Value v3(rapidjson::kArrayType);
-    m_run_config.AddMember("Cmd", v3, m_json_allocator);
-    rapidjson::Value v4;
-    m_run_config.AddMember("Dns", v4, m_json_allocator);
-    m_run_config.AddMember("Image", m_image.data(), m_json_allocator);
-    rapidjson::Value v5(rapidjson::kObjectType);
-    m_run_config.AddMember("Volumes", v5, m_json_allocator);
-    rapidjson::Value empty_object(rapidjson::kObjectType);
-    m_run_config["Volumes"].AddMember(m_runtime_path.c_str(), empty_object, m_json_allocator);
-    m_run_config.AddMember("VolumesFrom", "", m_json_allocator);
-    m_run_config.AddMember("WorkingDir", "/", m_json_allocator);
 }
 
 docker_t::~docker_t() {
@@ -169,55 +173,63 @@ docker_t::~docker_t() {
 
 void
 docker_t::spool() {
-    if(m_do_pool) {
-        m_docker_client.pull_image(m_image, m_tag);
+    try {
+        if(m_do_pool) {
+            m_docker_client.pull_image(m_image, m_tag);
+        }
+    } catch(const std::exception& e) {
+        throw cocaine::error_t("%s", e.what());
     }
 }
 
 std::unique_ptr<api::handle_t>
 docker_t::spawn(const std::string& path, const api::string_map_t& args, const api::string_map_t& environment) {
-    // prepare request to docker
-    auto& env = m_run_config["Env"];
-    env.SetArray();
-    for(auto it = environment.begin(); it != environment.end(); ++it) {
-        env.PushBack((it->first + "=" + it->second).c_str(), m_json_allocator);
-    }
-
-    auto& cmd = m_run_config["Cmd"];
-
-    cmd.SetArray();
-    cmd.PushBack(path.c_str(), m_json_allocator);
-
-    fs::path endpoint = fs::path(m_runtime_path);
-    for (auto it = args.begin(); it != args.end(); ++it) {
-        cmd.PushBack(it->first.c_str(), m_json_allocator);
-        if(it->first == "--endpoint") {
-            endpoint /= fs::path(it->second).filename();
-#if BOOST_VERSION >= 104600
-            cmd.PushBack(endpoint.native().c_str(), m_json_allocator);
-#else
-            cmd.PushBack(endpoint.string().c_str(), m_json_allocator);
-#endif
-        } else {
-            cmd.PushBack(it->second.c_str(), m_json_allocator);
+    try {
+        // prepare request to docker
+        auto& env = m_run_config["Env"];
+        env.SetArray();
+        for(auto it = environment.begin(); it != environment.end(); ++it) {
+            env.PushBack((it->first + "=" + it->second).c_str(), m_json_allocator);
         }
-    }
 
-    std::vector<std::string> binds;
+        auto& cmd = m_run_config["Cmd"];
+
+        cmd.SetArray();
+        cmd.PushBack(path.c_str(), m_json_allocator);
+
+        fs::path endpoint = fs::path(m_runtime_path);
+        for (auto it = args.begin(); it != args.end(); ++it) {
+            cmd.PushBack(it->first.c_str(), m_json_allocator);
+            if(it->first == "--endpoint") {
+                endpoint /= fs::path(it->second).filename();
 #if BOOST_VERSION >= 104600
-    std::string socket_dir(fs::path(args.at("--endpoint")).remove_filename().native().c_str());
+                cmd.PushBack(endpoint.native().c_str(), m_json_allocator);
 #else
-    std::string socket_dir(fs::path(args.at("--endpoint")).remove_filename().string().c_str());
+                cmd.PushBack(endpoint.string().c_str(), m_json_allocator);
 #endif
-    binds.emplace_back((socket_dir + ":" + m_runtime_path).c_str());
+            } else {
+                cmd.PushBack(it->second.c_str(), m_json_allocator);
+            }
+        }
 
-    // create container
-    std::unique_ptr<container_handle_t> handle(
-        new container_handle_t(m_docker_client.create_container(m_run_config))
-    );
+        std::vector<std::string> binds;
+#if BOOST_VERSION >= 104600
+        std::string socket_dir(fs::path(args.at("--endpoint")).remove_filename().native().c_str());
+#else
+        std::string socket_dir(fs::path(args.at("--endpoint")).remove_filename().string().c_str());
+#endif
+        binds.emplace_back((socket_dir + ":" + m_runtime_path).c_str());
 
-    handle->start(binds);
-    handle->attach();
+        // create container
+        std::unique_ptr<container_handle_t> handle(
+            new container_handle_t(m_docker_client.create_container(m_run_config))
+        );
 
-    return std::move(handle);
+        handle->start(binds);
+        handle->attach();
+
+        return std::move(handle);
+    } catch(const std::exception& e) {
+        throw cocaine::error_t("%s", e.what());
+    }
 }
