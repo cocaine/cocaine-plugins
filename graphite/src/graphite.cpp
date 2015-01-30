5240b4a1f4e69ab8870764af79c5b814daa3b475
antmat <antmat@me.com>
2015-01-30 18:41:19 +0300

    Debianization for graphite plugin. Reorder fix in graphite_t constructor.

--- graphite/src/graphite.cpp
+++ graphite/src/graphite.cpp
@@ -89,9 +89,9 @@ graphite_cfg_t::graphite_cfg_t(const cocaine::dynamic_t& args) :
 {}
 
 graphite_t::graphite_t(context_t& context, asio::io_service& _asio, const std::string& name, const dynamic_t& args) :
-    asio(_asio),
     service_t(context, asio, name, args),
     dispatch<io::graphite_tag>(name),
+    asio(_asio),
     config(args),
     log(context.log("graphite")),
     timer(asio),
