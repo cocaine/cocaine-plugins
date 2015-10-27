d8c4882d66a627cbeb7f62fd26b383f83959175f
Dmitry Unkovsky <oil.crayons@gmail.com>
2015-10-27 23:42:34 +0300

    bump required API version

--- conductor/src/module.cpp
+++ conductor/src/module.cpp
@@ -7,7 +7,7 @@ using namespace cocaine::isolate;
 extern "C" {
     auto
     validation() -> api::preconditions_t {
-        return api::preconditions_t { COCAINE_MAKE_VERSION(0, 12, 0) };
+        return api::preconditions_t { COCAINE_MAKE_VERSION(0, 12, 2) };
     }
 
     void
