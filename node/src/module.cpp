88db2c310064936a3cbe38acba6864ea731488b3
Evgeny Safronov <division494@gmail.com>
2016-02-02 17:57:34 +0300

    fix(node): restrict version

--- node/src/module.cpp
+++ node/src/module.cpp
@@ -8,7 +8,7 @@ using namespace cocaine::service;
 extern "C" {
     auto
     validation() -> api::preconditions_t {
-        return api::preconditions_t { COCAINE_MAKE_VERSION(0, 12, 2) };
+        return api::preconditions_t { COCAINE_MAKE_VERSION(0, 12, 5) };
     }
 
     void
