778fa5ae5f6469591d5fb6f9ef35cbda33d0cb65
Evgeny Safronov <division494@gmail.com>
2016-02-09 16:42:21 +0300

    feat(echo): add echo plugin

---
+++ echo/src/module.cpp
@@ -0,0 +1,11 @@
+#include "cocaine/service/echo.hpp"
+
+extern "C" {
+    auto validation() -> cocaine::api::preconditions_t {
+        return cocaine::api::preconditions_t{COCAINE_MAKE_VERSION(0, 12, 0)};
+    }
+
+    auto initialize(cocaine::api::repository_t& repository) -> void {
+        repository.insert<cocaine::service::echo_t>("echo");
+    }
+}
