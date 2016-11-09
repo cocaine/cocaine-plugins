266c6ae226a4e4a82693d93a68e13c4b2966d6ed
Evgeny Safronov <division494@gmail.com>
2016-11-09 20:46:19 +0300

    refactor: minor renaming

--- node/src/api/auth.cpp
+++ node/src/api/auth.cpp
@@ -13,16 +13,16 @@ namespace api {
 
 namespace {
 
-const char COMPONENT_NAME[] = "authorizations";
+const char COMPONENT_GROUP_NAME[] = "authorizations";
 
 }  // namespace
 
 auto auth(context_t& context, const std::string& name) -> std::shared_ptr<auth_t> {
-    if (auto config = context.config().component_group(COMPONENT_NAME).get(name)) {
-        return context.repository().get<auth_t>(config->type(), context, name, config->args());
-    } else {
-        throw std::system_error(std::make_error_code(std::errc::argument_out_of_domain), name);
+    if (auto cfg = context.config().component_group(COMPONENT_GROUP_NAME).get(name)) {
+        return context.repository().get<auth_t>(cfg->type(), context, name, cfg->args());
     }
+
+    throw std::system_error(std::make_error_code(std::errc::argument_out_of_domain), name);
 }
 
 }  // namespace api
