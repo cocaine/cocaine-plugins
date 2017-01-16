b1c1a0ae2923f24c32aa1fc2ae5b68f7a2e29860
Evgeny Safronov <division494@gmail.com>
2017-01-16 19:17:53 +0300

    fix: fix memory corruption in metrics

--- node/src/node/stats.cpp
+++ node/src/node/stats.cpp
@@ -20,12 +20,14 @@ stats_t::stats_t(context_t& context, const std::string& name, std::chrono::high_
         context.metrics_hub().counter<std::int64_t>(cocaine::format("{}.slaves.crashed", name))
     },
     meter(context.metrics_hub().meter(cocaine::format("{}.rate", name))),
-    queue_depth(new metrics::usts::ewma_t(interval)),
+    queue_depth(std::make_shared<metrics::usts::ewma_t>(interval)),
     queue_depth_gauge(context.metrics_hub()
-        .register_gauge<double>(cocaine::format("{}.queue.depth_average", name), {}, [&]() -> double {
-            return queue_depth->get();
-        }
-    )),
+        .register_gauge<double>(
+            cocaine::format("{}.queue.depth_average", name),
+            {},
+            std::bind(&metrics::usts::ewma_t::get, queue_depth)
+        )
+    ),
     timer(context.metrics_hub().timer<metrics::accumulator::sliding::window_t>(cocaine::format("{}.timings", name)))
 {
     queue_depth->add(0);
